058a06006acf4a91c13b048e6a6bf0b06b42f0cc
Charles Sipe <charlessipe@Charless-MacBook-Pro.local>
2020-02-02 13:45:29 -0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Navigation />
+    <router-view class="container" :user="user" />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Navigation from "@/components/Navigation.vue";
+import Firebase from "firebase";
+//import db from "./db.js";
 
-#nav {
-  padding: 30px;
+// @ is an alias to /src
+//import Navigation from "@/components/Navigation.vue";
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: "App",
+  data: function(){
+    return {
+      user: null
     }
+  },
+  mounted(){
+    Firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.user = user.email;
+      }
+    })
+  },
+  components: {
+    Navigation
   }
-}
+};
+</script>
+
+
+<style lang="scss">
+$primary: #05b2dd;
+@import "node_modules/bootstrap/scss/bootstrap";
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <div class="navbar navbar-expand bg-primary navbar-dark">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">Ranked Blogs</router-link>
+    </div>
+    <div class="navbar-nav ml-auto">
+      <router-link class="nav-item nav-link" to="/login">Login</router-link>
+    </div>
+  </div>
+</template>
